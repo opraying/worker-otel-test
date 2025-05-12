eb560873aba5ef2ae6634185dc219a5e60203607
Ray <uek0819@gmail.com>
2025-05-12 14:02:39 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-import * as Resource from "@effect/opentelemetry/Resource"
-import * as Tracer from "@effect/opentelemetry/Tracer"
-import * as Etag from "@effect/platform/Etag"
-import * as FileSystem from "@effect/platform/FileSystem"
+import * as Otlp from "@effect/opentelemetry/Otlp"
+import * as FetchHttpClient from "@effect/platform/FetchHttpClient"
 import * as HttpApiBuilder from "@effect/platform/HttpApiBuilder"
-import * as HttpPlatform from "@effect/platform/HttpPlatform"
-import * as Path from "@effect/platform/Path"
-import type { ResolveConfigFn } from "@microlabs/otel-cf-workers"
-import { instrument } from "@microlabs/otel-cf-workers"
+import * as HttpServer from "@effect/platform/HttpServer"
 import { pipe } from "effect/Function"
 import * as Layer from "effect/Layer"
-import * as Logger from "effect/Logger"
 import { MyHttpApi } from "./api"
 import { HttpAppLive } from "./handle"
 
@@ -19,51 +13,45 @@ declare global {
 }
 
 const SERVICE_NAME = "greetings"
-
-const TraceLive = Tracer.layerGlobal.pipe(
-  Layer.provide(Resource.layer({
-    serviceName: SERVICE_NAME
-  }))
-)
+const NAMESPACE = "workers-test"
+const DATASET = NAMESPACE
 
 const HttpLive = HttpApiBuilder.api(MyHttpApi).pipe(
-  //
   Layer.provide([HttpAppLive]),
-  Layer.provide(TraceLive)
+  Layer.provide(Layer.suspend(() => {
+    return Otlp.layer({
+      baseUrl: globalThis.env.OTEL_HOST ?? "http://localhost:4318",
+      resource: {
+        serviceName: SERVICE_NAME,
+        serviceVersion: "0.0.1"
+      },
+      headers: {
+        "x-api-key": globalThis.env.OTEL_API_KEY,
+        "x-namespace": NAMESPACE,
+        "x-service": SERVICE_NAME,
+        "x-baselime-dataset": DATASET
+      }
+    })
+  })),
+  Layer.provide(FetchHttpClient.layer)
 )
 
 const Live = pipe(
   HttpApiBuilder.Router.Live,
   Layer.provideMerge(HttpLive),
-  Layer.provideMerge(HttpPlatform.layer),
-  Layer.provideMerge(Etag.layerWeak),
-  Layer.provideMerge(Path.layer),
-  Layer.provideMerge(FileSystem.layerNoop({})),
-  Layer.provide(Logger.pretty),
-  Layer.provide(TraceLive)
+  Layer.provideMerge(HttpServer.layerContext)
 )
 
-const config: ResolveConfigFn = (_env: Env, _trigger) => {
-  return {
-    exporter: {
-      url: "http://localhost:4318/v1/traces",
-      headers: {}
-    },
-    sampling: {
-      headSampler: { ratio: 1 }
-    },
-    service: { name: SERVICE_NAME }
-  }
-}
-
-export default instrument({
-  fetch(request: Request, env: Env, _ctx: ExecutionContext) {
+export default {
+  fetch(request, env, ctx) {
     Object.assign(globalThis, {
       env
     })
 
     const handler = HttpApiBuilder.toWebHandler(Live)
 
-    return handler.handler(request as unknown as Request)
+    return handler.handler(request as unknown as Request).finally(() => {
+      ctx.waitUntil(handler.dispose())
+    })
   }
-}, config)
+} satisfies ExportedHandler<Env>
